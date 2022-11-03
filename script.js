9f0652ba6ac0f98c1a331e3166684114a9f44cbc
Giulia Amaral <75877954+GiuliaAmaral@users.noreply.github.com>
2022-11-03 17:16:41 -0300

    função compartilhar

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+inicio();
+
+async function inicio() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.get("texto")) {
+        document.querySelector('#texto').value = urlParams.get("texto");
+        gerarQRCode();
+    }
+}
+
+
 function gerarQRCode() {
     window.document.querySelector('#carregando').classList.remove("d-none");
     window.document.querySelector('#gerouQR').classList.add("d-none");
-    let textAreaUsuario = document.querySelector('textarea').value;
+    let textAreaUsuario = document.querySelector('#texto').value;
     let APIQRCode = `https://chart.googleapis.com/chart?cht=qr&chs=380x380&chl=${textAreaUsuario}`;
     window.document.querySelector('#imagemQRCode').src = APIQRCode;
     setTimeout(() => {
@@ -11,16 +23,38 @@ function gerarQRCode() {
 
 }
 
-function imprimir() {
+async function compartilhar() {
+    let urlAtual = window.location.href;
+    const texto = window.document.querySelector('#texto').value;
+    const urlImg = window.document.querySelector('#imagemQRCode').src;
 
-    let conteudo = document.getElementById('gerouQR').innerHTML;
-    let tela_impressao = window.open();
+    const fileName = 'qrcode.png'
+    const response = await fetch(urlImg);
+    const contentType = response.headers.get('content-type')
+    const blob = await response.blob()
+    const file = new File([blob], fileName, { type: contentType })
+    const files = Array(file);
 
+    urlAtual = urlAtual.split("?")[0];
 
-    tela_impressao.document.write(conteudo);
-    tela_impressao.print();
-    // tela_impressao.window.close();
+    window.navigator.share({
+        url: `${urlAtual}?texto=${texto}`,
+        text: `Acesse o QRCode`,
+        files
+    });
 
 }
+
+// function imprimir() {
+
+//     let conteudo = document.getElementById('gerouQR').innerHTML;
+//     let tela_impressao = window.open();
+
+
+//     tela_impressao.document.write(conteudo);
+//     tela_impressao.print();
+//     // tela_impressao.window.close();
+
+// }
 
 
